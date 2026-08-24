a76af1fdf168ec807eb047c490fd38b6c5bedc4e
GuiPnj <guipynj@gmail.com>
2026-08-24 19:47:57 -0300

    feat: add BasicCalculator class
    Encapsulates operations inside the BasicCalculator class

--- BasicCalculator.py
+++ BasicCalculator.py
@@ -1,6 +1,6 @@
 '''
 BasicCalculator class: 
-    Central of Operations
+    Operations Central
 '''
 class BasicCalculator:
     def __init__(self, n1, n2):
@@ -21,25 +21,19 @@ class BasicCalculator:
 
     def exponential(self, n1, n2):
         return n1**n2
-            
-'''
-1 - Funcionar
-2 - POO
-3 - Try catch (validar)
-'''
 
 result: float = 0
 BC = BasicCalculator(0, 0) 
 
 while True:
     print("\n*** Bem-vindo(a) a BasicCalculator!***\n"
-          "\t1 - Soma\n"
-          "\t2 - Subtração\n"
-          "\t3 - Multiplicação\n"
-          "\t4 - Divisão\n"
-          "\t5 - Elevar\n"
-          "\t0 - Sair\n"
-          )
+            "\t1 - Soma\n"
+            "\t2 - Subtração\n"
+            "\t3 - Multiplicação\n"
+            "\t4 - Divisão\n"
+            "\t5 - Elevar\n"
+            "\t0 - Sair\n"
+        )
     menuKey = int(input("Selecione uma operação: "))
     if not menuKey:
         break
@@ -60,6 +54,7 @@ while True:
             result = BC.exponential(number1, number2)           
         case _:
            print("Valor de seleção inválido")
+           break
           
     print(f"\t*** O resultado da operação é: {result}***\n")
 
